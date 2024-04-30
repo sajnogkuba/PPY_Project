4f284192b24d820be8d6bc85d03b7ddf468f9556
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-30 14:28:43 +0200

    add methods to TaskManager class

---
+++ Projekt_PPY/Classes/TaskManager.py
@@ -0,0 +1,24 @@
+class TaskManager:
+    def __init__(self, file):
+        self.file = file
+        self.tasks = []
+
+    def addTask(self):
+        # TODO
+        pass
+
+    def deleteTask(self):
+        # TODO
+        pass
+
+    def editTask(self):
+        # TODO
+        pass
+
+    def filterTasks(self):
+        # TODO
+        pass
+
+    def viewTasks(self):
+        # TODO
+        pass
--- Projekt_PPY/Main.py
+++ Projekt_PPY/Main.py
@@ -11,14 +11,19 @@ while not stop:
     selected_action = user_interface.selectAction(file_handler.file)
     match selected_action:
         case Actions.ADD_TASK:
+            # TODO implement methods to add tasks
             pass
         case Actions.DELETE_TASK:
+            # TODO implement methods to delete tasks
             pass
         case Actions.EDIT_TASK:
+            # TODO implement methods to edit tasks
             pass
         case Actions.FILTER_TASKS:
+            # TODO implement methods to filter tasks
             pass
         case Actions.VIEW_TASKS:
+            # TODO implement methods to view tasks
             pass
         case Actions.CHANGE_FILE:
             file_handler.SelectFile()
