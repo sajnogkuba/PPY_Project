a6dcdc8c20a295615c813ff319d0ac49cf0bbe5b
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-05-23 18:20:49 +0200

    add documentation to Exceptions

--- Projekt_PPY/Exceptions/InvalidDateError.py
+++ Projekt_PPY/Exceptions/InvalidDateError.py
@@ -1,5 +1,17 @@
 class InvalidDateError(Exception):
+    """
+    Exception raised for invalid date formats or dates.
+
+    Attributes:
+        message (str): Explanation of the error.
+    """
     def __init__(self, message):
+        """
+        Initializes an instance of InvalidDateError.
+
+        Args:
+            message (str): Explanation of the error.
+        """
         self.message = message
         super().__init__(self.message)
 
--- Projekt_PPY/Exceptions/InvalidSelectError.py
+++ Projekt_PPY/Exceptions/InvalidSelectError.py
@@ -1,4 +1,16 @@
 class InvalidSelectError(Exception):
+    """
+    Exception raised for invalid selections made by the user.
+
+    Attributes:
+        message (str): Explanation of the error.
+    """
     def __init__(self, message):
+        """
+        Initializes an instance of InvalidSelectError.
+
+        Args:
+            message (str): Explanation of the error.
+        """
         self.message = message
         super().__init__(self.message)
