211382b9b910c584a6c3903f1921197cf6cc6aa8
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-05-23 13:56:27 +0200

    add new fields to task

--- Projekt_PPY/Classes/Task.py
+++ Projekt_PPY/Classes/Task.py
@@ -5,16 +5,21 @@ from Enums.Status import Status
 
 
 class Task:
-    def __init__(self, name: str, description: str, priority: Priority, deadline: datetime.datetime, status: Status):
+    def __init__(self, name: str, description: str, priority: Priority, category: str, deadline: datetime.datetime, status: Status):
         self.name = name
         self.description = description
         self.priority = priority
+        self.category = category
         self.deadline = deadline
         self.status = status
+        self.created_at = datetime.datetime.now()
+        self.completed_at = '-'
 
     def __str__(self):
         return (f'Name: {self.name},'
                 f' Description: {self.description},'
                 f' Priority: {self.priority.name}, '
                 f' Deadline: {self.deadline}' 
-                f' Status: {self.status.name}')
+                f' Status: {self.status.name}'
+                f' Created at: {self.created_at}'
+                f' completed at: {self.completed_at}')
