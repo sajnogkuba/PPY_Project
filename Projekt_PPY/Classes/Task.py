0f519b36794766881927c8fe6a5b416e256a77e6
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-05-23 17:57:23 +0200

    add documentation to class Task

--- Projekt_PPY/Classes/Task.py
+++ Projekt_PPY/Classes/Task.py
@@ -1,11 +1,34 @@
 import datetime
-
 from Enums.Priority import Priority
 from Enums.Status import Status
 
 
 class Task:
+    """
+    Represents a task with various attributes.
+
+    Attributes:
+        name (str): The name of the task.
+        description (str): Description of the task.
+        priority (Priority): Priority level of the task (from Enums.Priority).
+        category (str): Category of the task.
+        deadline (datetime.datetime): Deadline of the task.
+        status (Status): Current status of the task (from Enums.Status).
+        created_at (datetime.datetime): Timestamp of when the task was created.
+        completed_at (datetime.datetime or str): Timestamp of when the task was completed, or '-' if not completed.
+    """
     def __init__(self, name: str, description: str, priority: Priority, category: str, deadline: datetime.datetime, status: Status):
+        """
+        Initializes an instance of Task.
+
+        Args:
+            name (str): The name of the task.
+            description (str): Description of the task.
+            priority (Priority): Priority level of the task (from Enums.Priority).
+            category (str): Category of the task.
+            deadline (datetime.datetime): Deadline of the task.
+            status (Status): Current status of the task (from Enums.Status).
+        """
         self.name = name
         self.description = description
         self.priority = priority
@@ -18,6 +41,12 @@ class Task:
             self.completed_at = datetime.datetime.now()
 
     def __str__(self):
+        """
+        Returns a string representation of the task.
+
+        Returns:
+            str: String representation of the task.
+        """
         return (f'Name: {self.name} '
                 f' Description: {self.description} '
                 f' Priority: {self.priority.name} '
